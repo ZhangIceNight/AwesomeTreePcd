cd5665d358a98ee15ed00f228a58f13a809acdb7
ZhangIceNight <a597536806@163.com>
2025-07-19 13:21:59 +0800

    Update at 2025-07-19 13:21:59

--- models/pointnet_pl.py
+++ models/pointnet_pl.py
@@ -38,41 +38,41 @@ class PointCloudModel(pl.LightningModule):
         }
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), lr=self.opt_hparams["learning_rate"], weight_decay=self.opt_hparams["weight_decay"])
+        # optimizer = optim.AdamW(self.parameters(), lr=self.opt_hparams["learning_rate"], weight_decay=self.opt_hparams["weight_decay"])
  
-        total_epochs = self.opt_hparams["max_epochs"]  # 例如：50
-        warmup_epochs = self.opt_hparams["warmup_epochs"]
+        # total_epochs = self.opt_hparams["max_epochs"]  # 例如：50
+        # warmup_epochs = self.opt_hparams["warmup_epochs"]
 
-        scheduler_warmup = LinearLR(
-            optimizer,
-            start_factor=0.1,
-            end_factor=1.0,
-            total_iters=warmup_epochs
-        )
+        # scheduler_warmup = LinearLR(
+        #     optimizer,
+        #     start_factor=0.1,
+        #     end_factor=1.0,
+        #     total_iters=warmup_epochs
+        # )
     
-        # Cosine 退火阶段：从 max lr 衰减到接近 0
-        scheduler_cosine = CosineAnnealingLR(
-            optimizer,
-            T_max=total_epochs - warmup_epochs,  
-            eta_min=self.opt_hparams["eta_min"]
-        )
+        # # Cosine 退火阶段：从 max lr 衰减到接近 0
+        # scheduler_cosine = CosineAnnealingLR(
+        #     optimizer,
+        #     T_max=total_epochs - warmup_epochs,  
+        #     eta_min=self.opt_hparams["eta_min"]
+        # )
     
-        # 合并两个调度器为一个阶段式调度器
-        combined_scheduler = SequentialLR(
-            optimizer,
-            schedulers=[scheduler_warmup, scheduler_cosine],
-            milestones=[warmup_epochs]  # 第 5 个 epoch 结束后切换到 cosine
-        )
+        # # 合并两个调度器为一个阶段式调度器
+        # combined_scheduler = SequentialLR(
+        #     optimizer,
+        #     schedulers=[scheduler_warmup, scheduler_cosine],
+        #     milestones=[warmup_epochs]  # 第 5 个 epoch 结束后切换到 cosine
+        # )
  
-        return {
-            'optimizer': optimizer,
-            'lr_scheduler': {
-                'scheduler': combined_scheduler,
-                'interval': 'epoch', 
-                'frequency': 1
-            }
-        }
- 
+        # return {
+        #     'optimizer': optimizer,
+        #     'lr_scheduler': {
+        #         'scheduler': combined_scheduler,
+        #         'interval': 'epoch', 
+        #         'frequency': 1
+        #     }
+        # }
+        return optim.Adam(self.parameters(), lr=self.opt_hparams["learning_rate"])
    
 
  
