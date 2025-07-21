470985b72590d6938f46cac7b261a298d419c227
ZhangIceNight <a597536806@163.com>
2025-07-21 14:39:25 +0800

    Update at 2025-07-21 14:39:25

---
+++ models/pointnet2.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from models.pointnet2_utils import PointNetSetAbstraction,PointNetFeaturePropagation
+
+
+class PointNet2(nn.Module):
+    # 1K input size
+    def __init__(self, cls_dim):
+        super(PointNet2, self).__init__()
+        self.sa1 = PointNetSetAbstraction(512, 0.2, 32, 6, [64, 64, 128], False)
+        self.sa2 = PointNetSetAbstraction(128, 0.4, 64, 128 + 3, [128, 128, 256], False)
+        self.sa3 = PointNetSetAbstraction(None, None, None, 256 + 3, [256, 512, 1024], True)
+        
+        self.fp3 = PointNetFeaturePropagation(1280, [256, 256])
+        self.fp2 = PointNetFeaturePropagation(384, [256, 128])
+        self.fp1 = PointNetFeaturePropagation(131, [128, 128, 128])
+        self.conv1 = nn.Conv1d(128, 128, 1)
+        self.bn1 = nn.BatchNorm1d(128)
+        self.drop1 = nn.Dropout(0.5)
+        self.conv2 = nn.Conv1d(128, cls_dim, 1)
+
+    def forward(self, xyz):
+        # Set Abstraction layers
+        B,C,N = xyz.shape
+
+        l0_points = xyz
+        l0_xyz = xyz
+
+        l1_xyz, l1_points = self.sa1(l0_xyz, l0_points)
+        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
+        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
+        # Feature Propagation layers
+        l2_points = self.fp3(l2_xyz, l3_xyz, l2_points, l3_points)
+        l1_points = self.fp2(l1_xyz, l2_xyz, l1_points, l2_points)
+        l0_points = self.fp1(l0_xyz, l1_xyz, l0_points, l1_points)
+        # FC layers
+        feat =  F.relu(self.bn1(self.conv1(l0_points)))
+        x = self.drop1(feat)
+        x = self.conv2(x)
+        x = F.log_softmax(x, dim=1)
+        x = x.permute(0, 2, 1)
+        return x
